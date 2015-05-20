78e1fd8a0e04f31c2ff1600c0c00ae162e6c2033
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2015-05-20 16:09:45 +0900

    Add dependecy to build_config.rb for travis ci

--- .travis_build_config.rb
+++ .travis_build_config.rb
@@ -2,4 +2,6 @@ MRuby::Build.new do |conf|
   toolchain :gcc
   conf.gembox 'default'
   conf.gem '../mruby-eventfd'
+  conf.gem :github => 'iij/mruby-process'
+  conf.gem :github => 'matsumoto-r/mruby-sleep'
 end
